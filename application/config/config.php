6570abc7899a29609c59d232dab971c031460670
M. Nur Fawaiq <nurfawaiq@gmail.com>
2023-12-21 23:24:34 +0700

    update support php 8.3.x

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mypos/';
+// $config['base_url'] = 'http://localhost/mypos/';
+$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://").$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
